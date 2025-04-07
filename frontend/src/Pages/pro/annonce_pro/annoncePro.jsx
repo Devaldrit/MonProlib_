5046a4460fae6c3d3fdb87d3c4d220ccf175610e
Aldrit <aldritkuleta2@gmail.com>
2025-04-07 15:42:26 +0200

    dev rajout du footer sur la page

--- frontend/src/Pages/pro/annonce_pro/annoncePro.jsx
+++ frontend/src/Pages/pro/annonce_pro/annoncePro.jsx
@@ -1,12 +1,13 @@
 import "./annoncePro.css";
 import HeaderConnexion from "../../../Component/header_connexion/headerConnexion";
 import Annonce from "../../../Component/Annonce/annonce.jsx";
-
+import Footer from "../../../Component/Footer/footer.jsx";
 function AnonncePro() {
   return (
     <div>
       <HeaderConnexion />
       <Annonce />
+      <Footer />
     </div>
   );
 }
