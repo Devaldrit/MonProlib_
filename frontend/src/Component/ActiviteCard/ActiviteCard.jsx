f022b5a23651899e0eeac97fdc9c6d23655bf5f7
Aldrit <aldritkuleta2@gmail.com>
2025-04-23 16:03:57 +0200

    fix actvite

--- frontend/src/Component/ActiviteCard/ActiviteCard.jsx
+++ frontend/src/Component/ActiviteCard/ActiviteCard.jsx
@@ -3,18 +3,12 @@ import "./ActiviteCard.css";
 
 const ActiviteCard = ({ titre, description }) => {
   return (
-    <div>
+    <div className="activiteCard">
       <div className="top">
         <h2 className="titleActivity">{titre}</h2>
         <span>-</span>
         <p className="descriptionAcitvity">{description}</p>
       </div>
-import "./activiteCard.css";
-
-const ActiviteCard = ({ name }) => {
-  return (
-    <div className="activityCard">
-      <p>{name}</p>
     </div>
   );
 };
